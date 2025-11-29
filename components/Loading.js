5cb4786bbfff2877f6dd66aea55d74af5836444c
Lovenish <heelme1181@gmail.com>
2025-11-29 15:55:03 +0530

    making ssr changes

--- components/Loading.js
+++ components/Loading.js
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <span className="flex flex-col items-center gap-3">
-        <ClipLoader size="70" color="#bbb4b4" />
+         <ClipLoader size="70px" color="#bbb4b4" />
          <span className="text-gray-500">Loading</span> 
       </span>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -4,30 +4,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistor, store } from "../store/store";
 import Loading from "../components/Loading";
 import { PersistGate } from "redux-persist/integration/react";
-import { useEffect, useState } from "react";
+import ClientOnly from "../components/ClientOnly";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
   return (
     <Provider store={store}>
-      {isClient ? (
-        <PersistGate loading={<Loading />} persistor={persistor}>
-          <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <ClientOnly fallback={<Loading />}>
+          <PersistGate loading={<Loading />} persistor={persistor}>
             <Component {...pageProps} />
-          </QueryClientProvider>
-        </PersistGate>
-      ) : (
-        <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
-        </QueryClientProvider>
-      )}
+          </PersistGate>
+        </ClientOnly>
+      </QueryClientProvider>
     </Provider>
   );
 }
