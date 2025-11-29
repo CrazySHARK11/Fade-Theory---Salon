b0556b7257d8fb41efaeac6224685a56987c0301
Lovenish <heelme1181@gmail.com>
2025-11-29 15:08:02 +0530

    added ssr modifications

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,30 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistor, store } from "../store/store";
 import Loading from "../components/Loading";
 import { PersistGate } from "redux-persist/integration/react";
+import { useEffect, useState } from "react";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
- 
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <Provider store={store}>
-      <PersistGate loading={<Loading />} persistor={persistor} >
+      {isClient ? (
+        <PersistGate loading={<Loading />} persistor={persistor}>
+          <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+          </QueryClientProvider>
+        </PersistGate>
+      ) : (
         <QueryClientProvider client={queryClient}>
           <Component {...pageProps} />
         </QueryClientProvider>
-      </PersistGate>
+      )}
     </Provider>
   );
 }
